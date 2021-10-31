42bf160d42b64ba90939f894087f6920001a2307
Prashant Sharma <prashantsharma161198@gmail.com>
2021-10-31 22:16:32 +0530

    feat(*): add experience section and its related data
    removed dark theme temporarily, as the experience timeline doesn't
    conform to the dark theme as of now.

---
+++ data/experienceData.js
@@ -0,0 +1,48 @@
+const experienceData = [
+  {
+    title: 'July 2020 - present',
+    cardTitle: 'Zyte',
+    cardSubtitle: 'Python Developer',
+    cardDetailedText: [
+      `Creating & Maintaining web spiders to fulfil web scraping requirements via Scrapy.`,
+      `Working with both Data Operations and Data Set Up teams to satisfy end to end scraping requirements.`,
+    ],
+  },
+  {
+    title: 'Aug 2020 - July 2021',
+    cardTitle: 'Attentive',
+    cardSubtitle: 'Software Developer Engineer (SDE-I)',
+    cardDetailedText: [
+      `Managed and created features for multiple microservices, and exposed myself to maintaining such components of softwares.`,
+      `Developed backend for the major product of the company from scratch. Went through various phases of Software Development. Also created
+      Geodjango based project that leverage OSM data for US roads.`,
+      `Database & API Designing, API structuring & conventions, backend architecture, Asynchronous Programming, implementing producer
+      consumer model are few things learnt through the process of developing the products.`,
+      `Maintained & created various scripts for data fetching for multiple analytical & growth purposes.`,
+      `Acquired in-depth knowlege in GIS domain & its related tools - QGIS, Vector Data Manipulation(Geojson, KML, Shapefiles), GDAL etc.`,
+    ],
+  },
+  {
+    title: 'Jan 2020 - Apr 2020',
+    cardTitle: 'Attentive',
+    cardSubtitle: 'Backend Developer Intern',
+    cardDetailedText: [
+      `As a Backend developer, I have gotten the opportunity to create end-user products and experience various phases of SDLC.`,
+      `Developed REST APIs in DRF, for an internal tool, used for street annotation. Implemented test coverage up to 82% by writing tests using Pytest.`,
+      `Leveraged knowledge in GIS specific terminologies and backed a landscape specific feature platform by creating REST APIs.
+       Involved in the product development from database designing to implementing crucial features.`,
+    ],
+  },
+  {
+    title: 'Jun 2019 - Aug 2019',
+    cardTitle: 'Attentive',
+    cardSubtitle: 'Software Developer Intern',
+    cardDetailedText: [
+      `My responsibilities include developing REST API using Django's REST framework as well as refactoring already available API code.`,
+      `Worked on the back-end development of "Bumpp", a location based social mobile application which included creating API endpoints for different services`,
+      `Created Rest APIs for supporting back-end of "Mapx", a platform for creating requests for Feature Extraction and helps to track those request.`,
+    ],
+  },
+]
+
+export default experienceData

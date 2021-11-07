1edf010c984a54da04bddc7ec9cbbb03fd39aa8e
Prashant Sharma <prashantsharma161198@gmail.com>
2021-11-07 23:15:10 +0530

    refactor(components, pages/): add divider & use pageTitle for titles

--- components/Skills.js
+++ components/Skills.js
@@ -1,24 +1,26 @@
 const Skills = ({ skillCategoryData }) => {
   return (
     <div className="prose dark:prose-dark lg:prose-xl">
-      {skillCategoryData.map((category) => (
-        <div key={category.name}>
-          <h4>{category.name}</h4>
-          <div className="grid grid-cols-3">
-            {category?.skills?.map((skill) => (
-              <div className="space-y-4" key={skill}>
-                <div className="flex items-center text-base font-medium">
-                  <img
-                    src="https://img.icons8.com/material-outlined/24/000000/black-and-white.png"
-                    alt="black-and_white-bullet"
-                  />
-                  <span className="ml-2">{skill}</span>
+      <div className="space-y divide-y divide-gray-200 dark:divide-gray-800">
+        {skillCategoryData.map((category) => (
+          <div key={category.name}>
+            <h4>{category.name}</h4>
+            <div className="grid grid-cols-3">
+              {category?.skills?.map((skill) => (
+                <div className="space-y-4" key={skill}>
+                  <div className="flex items-center text-base font-medium">
+                    <img
+                      src="https://img.icons8.com/material-outlined/24/000000/black-and-white.png"
+                      alt="black-and_white-bullet"
+                    />
+                    <span className="ml-2">{skill}</span>
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   )
 }
--- pages/timeline.js
+++ pages/timeline.js
@@ -1,35 +1,41 @@
 import React from 'react'
 import { Chrono } from 'react-chrono'
 import timelineData from '@/data/timelineData'
+import PageTitle from '@/components/PageTitle'
 
-export default function Experience() {
+export default function Timeline() {
   return (
-    <div style={{ width: '100%', height: '100%' }}>
-      <Chrono
-        items={timelineData}
-        mode="VERTICAL_ALTERNATING"
-        theme={{
-          primary: 'black',
-          secondary: 'skyblue',
-          cardBgColor: 'white',
-          cardForeColor: 'brown',
-          titleColor: 'black',
-        }}
-        cardHeight="100"
-      >
-        <div className="chrono-icons">
-          <img src="https://img.icons8.com/color/48/000000/briefcase.png" alt="briefcase" />
-          <img src="https://img.icons8.com/color/48/000000/trophy.png" alt="trophy" />
-          <img src="https://img.icons8.com/color/48/000000/briefcase.png" alt="briefcase" />
-          <img src="https://img.icons8.com/color/48/000000/briefcase.png" alt="briefcase" />
-          <img src="https://img.icons8.com/color/48/000000/briefcase.png" alt="briefcase" />
-          <img src="https://img.icons8.com/color/48/000000/training.png" alt="training" />
-          <img src="https://img.icons8.com/color/48/000000/training.png" alt="training" />
-          <img src="https://img.icons8.com/color/48/000000/prize.png" alt="prize" />
-          <img src="https://img.icons8.com/color/48/000000/university.png" alt="university" />
-          <img src="https://img.icons8.com/color/48/000000/training.png" alt="training" />
-        </div>
-      </Chrono>
+    <div className="divide-y divide-gray-200 dark:divide-gray-700">
+      <div className="pt-6 pb-8 space-y-2 md:space-y-5">
+        <PageTitle>Timeline</PageTitle>
+      </div>
+      <div style={{ width: '100%', height: '100%' }} className="py-7">
+        <Chrono
+          items={timelineData}
+          mode="VERTICAL_ALTERNATING"
+          theme={{
+            primary: 'black',
+            secondary: 'skyblue',
+            cardBgColor: 'white',
+            cardForeColor: 'brown',
+            titleColor: 'black',
+          }}
+          cardHeight="100"
+        >
+          <div className="chrono-icons">
+            <img src="https://img.icons8.com/color/48/000000/briefcase.png" alt="briefcase" />
+            <img src="https://img.icons8.com/color/48/000000/trophy.png" alt="trophy" />
+            <img src="https://img.icons8.com/color/48/000000/briefcase.png" alt="briefcase" />
+            <img src="https://img.icons8.com/color/48/000000/briefcase.png" alt="briefcase" />
+            <img src="https://img.icons8.com/color/48/000000/briefcase.png" alt="briefcase" />
+            <img src="https://img.icons8.com/color/48/000000/training.png" alt="training" />
+            <img src="https://img.icons8.com/color/48/000000/training.png" alt="training" />
+            <img src="https://img.icons8.com/color/48/000000/prize.png" alt="prize" />
+            <img src="https://img.icons8.com/color/48/000000/university.png" alt="university" />
+            <img src="https://img.icons8.com/color/48/000000/training.png" alt="training" />
+          </div>
+        </Chrono>
+      </div>
     </div>
   )
 }

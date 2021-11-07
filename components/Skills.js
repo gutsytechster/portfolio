2bc30bb3f730df63c63403309b9530832f7f3994
Prashant Sharma <prashantsharma161198@gmail.com>
2021-11-07 23:52:48 +0530

    fix(components/Skill.js): remove bullet icon & update text color

--- components/Skills.js
+++ components/Skills.js
@@ -1,21 +1,17 @@
 const Skills = ({ skillCategoryData }) => {
   return (
     <div className="prose dark:prose-dark lg:prose-xl">
-      <div className="space-y divide-y divide-gray-200 dark:divide-gray-800">
+      <div className="space-y divide-y divide-gray-200 dark:divide-gray-700">
         {skillCategoryData.map((category) => (
           <div key={category.name}>
             <h4>{category.name}</h4>
             <div className="grid grid-cols-3">
               {category?.skills?.map((skill) => (
-                <div className="space-y-4" key={skill}>
+                <ul className="space-y-4" key={skill}>
                   <div className="flex items-center text-base font-medium">
-                    <img
-                      src="https://img.icons8.com/material-outlined/24/000000/black-and-white.png"
-                      alt="black-and_white-bullet"
-                    />
-                    <span className="ml-2">{skill}</span>
+                    <span className="ml-2 text-gray-500 dark:text-gray-400">{skill}</span>
                   </div>
-                </div>
+                </ul>
               ))}
             </div>
           </div>
